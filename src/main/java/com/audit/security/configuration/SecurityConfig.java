247e58162339557a727ab4d3b0969ff53ea8e7ec
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-10-03 21:09:35 +0700

    add enable cors and disable csrf

--- src/main/java/com/audit/security/configuration/SecurityConfig.java
+++ src/main/java/com/audit/security/configuration/SecurityConfig.java
@@ -4,20 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+
+                .cors()
+
+                .and()
                 .authorizeRequests()
                 .anyRequest()
-                .permitAll();
+                .permitAll()
+
+                .and()
+                .csrf()
+                .disable();
 
         return http.build();
     }
