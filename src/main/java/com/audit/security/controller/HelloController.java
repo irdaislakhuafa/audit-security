4b83defbd39f916575fce933909a390f4396a6c1
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-10-08 02:01:51 +0700

    try protect hello endpoint

--- src/main/java/com/audit/security/controller/HelloController.java
+++ src/main/java/com/audit/security/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.audit.security.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PreAuthorize(value = "hasAnyAuthority('auditor')")
+@RequestMapping(value = { "/hello" })
 public class HelloController {
-    @RequestMapping(value = { "/" })
+    @RequestMapping
     public ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok().body("Hello World");
     }
